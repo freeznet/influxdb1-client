bd3b72602b58e32bb2e904e8594da52970851a42
Frankie O'Rourke <frankie@orourkedd.com>
2019-03-26 13:02:26 -0700

    convert to go module

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/orourkedd/influxdb1-client
+
+go 1.12
--- models/points_test.go
+++ models/points_test.go
@@ -12,7 +12,7 @@ import (
 	"testing"
 	"time"
 
-	"github.com/influxdata/influxdb1-client/models"
+	"github.com/orourkedd/influxdb1-client/models"
 )
 
 var (
